41325f1aa8c55df7913bd701eef62c0f558b401d
maria-angmat <maria.anguas.93@gmail.com>
2023-02-20 20:10:38 +0100

    add products page c and routing

--- shopeame/src/app/product-page/product-page-routing.module.ts
+++ shopeame/src/app/product-page/product-page-routing.module.ts
@@ -1,7 +1,12 @@
+import { ContainerComponent } from './container/container.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ContainerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- shopeame/src/app/product-page/product-page.module.ts
+++ shopeame/src/app/product-page/product-page.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductPageRoutingModule } from './product-page-routing.module';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ContainerComponent } from './container/container.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchBarComponent,
+    ProductsListComponent,
+    ContainerComponent
+  ],
   imports: [
     CommonModule,
     ProductPageRoutingModule
+  ],
+  exports: [
+    ContainerComponent
   ]
 })
 export class ProductPageModule { }
