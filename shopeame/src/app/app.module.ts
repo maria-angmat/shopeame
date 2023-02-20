e7d43cb9aae829f38248d9a70ef9bfe312f3d752
Sade112 <albertogauchia@gmail.com>
2023-02-20 19:25:33 +0100

    cambios 20/02

--- shopeame/src/app/app.module.ts
+++ shopeame/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './core/product/product.component';
 import { core } from '@angular/compiler';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
